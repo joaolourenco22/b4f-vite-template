28cf57501c6d6ae9cbce4bcfc3f2bfea080f9ec9
joaolourenco22 <joao_22lourenco@hotmail.com>
2025-09-22 15:03:36 +0100

    init tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      
-    </>
-  )
+    <div className="min-h-screen flex items-center justify-center bg-purple-700">
+      <h1 className="text-8xl font-extrabold text-white">
+        BYTES<span className="text-teal-500">4</span>FUTURE
+      </h1>
+    </div>
+  );
 }
 
-export default App
+export default App;
